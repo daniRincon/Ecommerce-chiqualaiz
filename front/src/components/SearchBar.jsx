437236f37e74724b6e691e14de0d6a3c7c3a3e17
Tativega <tatianavega89@gmail.com>
2019-11-07 16:52:09 -0300

    cambios

--- front/src/components/SearchBar.jsx
+++ front/src/components/SearchBar.jsx
@@ -2,18 +2,8 @@ import React from 'react';
 
 export default (props) =>{
     return (
-<<<<<<< HEAD
-        <form className='bar col-lg-9'>
-            <input size='30' 
-            className='form-control' 
-            type='search' 
-            placeholder='Search' 
-            name='search'>
-            </input>
-=======
         <form className='bar col-lg-7'>
             <input size='30' className='form-control' type='search' placeholder='Search' name='search'></input>
->>>>>>> 3c257c24bfde45aa7d4fa3d1cc7eabac41913139
         </form>
     )
 }
