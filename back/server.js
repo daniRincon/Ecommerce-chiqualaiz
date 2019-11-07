c2804da242939bbe95e64816ee9c8b3e8de66a73
Federico <fariganello@gmail.com>
2019-11-07 15:54:13 -0300

    add seeder

--- back/server.js
+++ back/server.js
@@ -1,13 +1,14 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
-const morgan = require('morgan');
-const indexRouter = require('./routes/index');
-const userRouter = require('./routes/index')
-const session = require('express-session');
-const passport = require('../back/config/passport')
-const db = require('./config/db');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/index");
+const session = require("express-session");
+const passport = require("../back/config/passport");
+const db = require("./config/db");
+const { Author, Genre, Book } = require("./models"); //NO BORRAR: Necesario para generar las relaciones y tablas al correr por primera vez
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
@@ -25,11 +26,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 //Passport
-app.use(session({ 
+app.use(
+  session({
     secret: "omdb",
     resave: false,
     saveUninitialized: true
-  }));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
