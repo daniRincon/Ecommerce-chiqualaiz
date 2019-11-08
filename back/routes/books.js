c6a60d41113533fbc710c046dfdfecf579296afc
Tativega <tatianavega89@gmail.com>
2019-11-08 14:05:19 -0300

    deshacer

--- back/routes/books.js
+++ back/routes/books.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const {fetchBooks, fetchBook} = require("../resolvers/books");
-
+const { fetchBooks, fetchBook } = require("../resolvers/books");
 
 router.get("/", fetchBooks);
 router.get("/:id", fetchBook);
-
-
 
 module.exports = router;
