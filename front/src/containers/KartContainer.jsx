e6aa7b57e2d085916f0624c7ba16c1758921b9d8
Federico <fariganello@gmail.com>
2019-11-11 12:44:49 -0300

    checkout finalizado

--- front/src/containers/KartContainer.jsx
+++ front/src/containers/KartContainer.jsx
@@ -2,6 +2,19 @@ import { connect } from "react-redux";
 import Kart from "../components/Kart";
 import { delCart, incCart, decCart } from "../store/actions/cart";
 
+const calculateTotal = arrayBook => {
+  return parseFloat(
+    arrayBook.reduce(
+      (total, book, index, cart) => total + Number(book[1] * cart[index][0]),
+      0
+    )
+  ).toFixed(2);
+};
+
+const handleClick = (total, history) => {
+  history.push("/checkout");
+};
+
 const mapStateToProps = state => ({
   cart: state.cart
 });
@@ -9,7 +22,9 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   delFromCart: id => dispatch(delCart(id)),
   handleDecrement: id => dispatch(decCart(id)),
-  handleIncrement: id => dispatch(incCart(id))
+  handleIncrement: id => dispatch(incCart(id)),
+  calculateTotal,
+  handleClick
 });
 
 export default connect(
