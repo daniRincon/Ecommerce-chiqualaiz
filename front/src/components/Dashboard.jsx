f226cd3e59bd4b07b945b13f0b84e08a1495767e
Federico <fariganello@gmail.com>
2019-11-12 07:34:19 -0300

    permite al admin agregar generos

--- front/src/components/Dashboard.jsx
+++ front/src/components/Dashboard.jsx
@@ -1,31 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import styles from '../css-modules/Dashboard.module.css'
+import styles from "../css-modules/Dashboard.module.css";
 
-export default (props) =>{
+export default props => {
+  useEffect(() => {
+    props.fetchBooks();
+  }, []);
 
-    useEffect(() => {
-        props.fetchBooks()
-    }, [])
-
-    if(props.authorized > 1){
+  if (props.authorized > 1) {
     return (
-       <div className={"btn-group-vertical " + styles.dashboard}>
-                <Link to="/dashboard/add"><button className={"btn btn-info " + styles.btn} >Agregar productos</button> </Link>
-                <button onClick={() => {
-                    props.history.push('/')}
-                    } className={"btn btn-info " + styles.btn}>Editar productos</button> 
-                <button className={"btn btn-info " + styles.btn}>Crear género</button> 
-                <button className={"btn btn-info " + styles.btn}>Órdenes</button> 
-                <button className={"btn btn-info " + styles.btn}>Editar usuarios</button> 
-                <button className={"btn btn-info " + styles.btn}>Editar categoría</button> 
-            </div>
-    )
-    } else {
-        return (
-            <h3 className="text-danger"> ACCESS DENIED</h3>
-        )
-    }
+      <div className={"btn-group-vertical " + styles.dashboard}>
+        <Link to="/dashboard/add">
+          <button className={"btn btn-info " + styles.btn}>
+            Agregar productos
+          </button>
+        </Link>
+        <button
+          onClick={() => {
+            props.history.push("/");
+          }}
+          className={"btn btn-info " + styles.btn}
+        >
+          Editar productos
+        </button>
+        <button
+          className={"btn btn-info " + styles.btn}
+          data-toggle="modal"
+          data-target="#modalGenre"
+        >
+          Agregar género
+        </button>
 
-}
-
+        <button className={"btn btn-info " + styles.btn}>Editar género</button>
+        <button className={"btn btn-info " + styles.btn}>Órdenes</button>
+        <button className={"btn btn-info " + styles.btn}>
+          Editar usuarios
+        </button>
+      </div>
+    );
+  } else {
+    return <h3 className="text-danger"> ACCESS DENIED</h3>;
+  }
+};
