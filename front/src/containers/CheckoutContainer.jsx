e536764d07df49a163892a54bfbfd28e434eb721
Acemanu412 <manuel.n.aceituno@gmail.com>
2019-11-15 07:50:49 -0300

    Arreglo mail

--- front/src/containers/CheckoutContainer.jsx
+++ front/src/containers/CheckoutContainer.jsx
@@ -38,12 +38,12 @@ class CheckoutContainer extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    let email = e.target[3].value ? e.target[1].value : e.target[0].value;
-    if (this.props.user.loggedName.id) {
-      this.state.socialNetworkUser
-        ? this.props.placeOrder(this.props.user.loggedName, email) &&
-          this.setState({ warning: "", orderPlaced: true })
-        : this.validPassword(this.state.password);
+    if(this.props.user.loggedName){
+    let email = e.target[3].value? e.target[1].value : e.target[0].value;
+    let cart = this.props.cart
+      this.state.socialNetworkUser? this.props.placeOrder(this.props.user.loggedName, email, cart)
+                                  && this.setState({ warning: "", orderPlaced: true })
+                                  : this.validPassword(this.state.password, email, cart);
     } else {
       return alert("Login required to purchase");
     } 
